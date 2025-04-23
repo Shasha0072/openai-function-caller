9e2fabdf33491342ea9dc8f8ef37a1df91fc9fc6
Shashwat Adhau <95584262+Shasha0072@users.noreply.github.com>
2025-04-23 11:11:32 +0530

    Add tools index file

---
+++ tools/index.js
@@ -0,0 +1,5 @@
+// Export all tools from a single file for easier imports
+export { weatherTool } from './weatherTool.js';
+export { newsTool } from './newsTool.js';
+
+// You can add more tool exports here as you create them
